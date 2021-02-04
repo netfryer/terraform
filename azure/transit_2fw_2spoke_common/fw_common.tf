e3b5ef141d07582932462174dc02c07660f3710e
netfryer <78517116+netfryer@users.noreply.github.com>
2021-02-03 20:46:09 -0600

    Update fw_common.tf

--- azure/transit_2fw_2spoke_common/fw_common.tf
+++ azure/transit_2fw_2spoke_common/fw_common.tf
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------------------------------------------
-# Create resource group for FWs, FW NICs, and FW LBs
+# Create resource group
 
 resource "azurerm_resource_group" "common_fw" {
   name     = "hub-ncus-palofw-rg"
@@ -26,6 +26,23 @@ module "common_fileshare" {
   local_file_path        = "bootstrap_files/"
 }
 
+#-----------------------------------------------------------------------------------------------------------------
+# Create Vnet
+  
+ resource "azurerm_resource_group" "transit" {
+  name     = "${var.global_prefix}${var.transit_prefix}-rg"
+  location = var.location
+}
+
+module "vnet" {
+  source              = "./modules/vnet/"
+  name                = "${var.transit_prefix}-vnet"
+  address_space       = var.transit_vnet_cidr
+  subnet_names        = var.transit_subnet_names
+  subnet_prefixes     = var.transit_subnet_cidrs
+  location            = var.location
+  resource_group_name = azurerm_resource_group.transit.name
+}
 
 #-----------------------------------------------------------------------------------------------------------------
 # Create VM-Series.  For every fw_name entered, an additional VM-Series instance will be deployed.
