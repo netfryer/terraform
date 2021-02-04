f8c9c6ad5e8d96b821f703dab3a9f4b36271bd1d
netfryer <78517116+netfryer@users.noreply.github.com>
2021-02-03 23:16:55 -0600

    Update variables.tf

--- azure/transit_2fw_2spoke_common/variables.tf
+++ azure/transit_2fw_2spoke_common/variables.tf
@@ -1,1 +1,85 @@
+variable location {
+  description = "Enter a location"
+}
 
+variable rg_target {
+}
+
+variable fw_prefix {
+  description = "Prefix to add to all resources added in the firewall resource group"
+  default     = ""
+}
+
+variable fw_license {
+  description = "VM-Series license: byol, bundle1, or bundle2"
+  # default = "byol"   
+  # default = "bundle1"  
+  # default = "bundle2"
+}
+
+variable global_prefix {
+  description = "Prefix to add to all resource groups created.  This is useful to create unique resource groups within a shared Azure subscription"
+}
+#-----------------------------------------------------------------------------------------------------------------
+# Transit VNET variables
+
+variable transit_prefix {
+}
+
+variable vnet_cidr {
+}
+
+variable transit_subnet_names {
+  type = list(string)
+}
+
+variable transit_subnet_cidrs {
+  type = list(string)
+}
+
+#-----------------------------------------------------------------------------------------------------------------
+# VM-Series variables
+
+variable fw_count {
+}
+
+variable fw_nsg_prefix {
+}
+
+variable fw_panos {
+}
+
+variable fw_username {
+}
+
+variable fw_password {
+}
+
+variable fw_internal_lb_ip {
+}
+
+variable fw_license {
+}
+
+#-----------------------------------------------------------------------------------------------------------------
+# Azure environment variables
+
+variable client_id {
+  description = "Azure client ID"
+  default = ""
+}
+
+variable client_secret {
+  description = "Azure client secret"
+  default = ""
+}
+
+variable subscription_id {
+  description = "Azure subscription ID"
+  default = ""
+}
+
+variable tenant_id {
+  description = "Azure tenant ID"
+  default = ""
+}
