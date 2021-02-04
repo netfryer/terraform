a11496b9a7f5b46e9d7dabdcc9400c0683a869c1
netfryer <78517116+netfryer@users.noreply.github.com>
2021-02-03 22:52:06 -0600

    Update fw_vnet.tf

--- azure/transit_2fw_2spoke_common/fw_vnet.tf
+++ azure/transit_2fw_2spoke_common/fw_vnet.tf
@@ -1,10 +1,3 @@
-#-----------------------------------------------------------------------------------------------------------------
-# Create Transit VNET
-#resource "azurerm_resource_group" "transit" {
-#  name     = "${var.global_prefix}${var.transit_prefix}-rg"
-#  location = var.location
-#}
-
 #module "vnet" {
 #  source              = "./modules/vnet/"
 #  name                = "${var.transit_prefix}-vnet"
@@ -12,5 +5,5 @@
 #  subnet_names        = var.transit_subnet_names
 #  subnet_prefixes     = var.transit_subnet_cidrs
 #  location            = var.location
-#  resource_group_name = azurerm_resource_group.transit.name
+#  resource_group_name = var.rg_target
 #}
