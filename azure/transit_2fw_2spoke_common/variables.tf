7aa6740bc21b96494562a738c65c3fd064bfb0ed
netfryer <78517116+netfryer@users.noreply.github.com>
2021-02-03 23:17:45 -0600

    Update variables.tf

--- azure/transit_2fw_2spoke_common/variables.tf
+++ azure/transit_2fw_2spoke_common/variables.tf
@@ -10,13 +10,6 @@ variable fw_prefix {
   default     = ""
 }
 
-variable fw_license {
-  description = "VM-Series license: byol, bundle1, or bundle2"
-  # default = "byol"   
-  # default = "bundle1"  
-  # default = "bundle2"
-}
-
 variable global_prefix {
   description = "Prefix to add to all resource groups created.  This is useful to create unique resource groups within a shared Azure subscription"
 }
