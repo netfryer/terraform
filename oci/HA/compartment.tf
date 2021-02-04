4419137d06b509d66196e386077c227e78ee0634
netfryer <78517116+netfryer@users.noreply.github.com>
2021-02-03 19:48:59 -0600

    Delete oci directory

--- oci/HA/compartment.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "compartment" {
-  compartment_id = "${var.parent_compartment_ocid}"
-  name           = "${var.compartment_name}"
-  description    = "compartment created by terraform"
-  enable_delete  = true
-}
--- oci/HA/internet_gateway.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_core_internet_gateway" "internet_gateway" {
-  compartment_id = "${oci_identity_compartment.compartment.id}"
-  vcn_id = "${oci_core_vcn.vcn.id}"
-  display_name = "IG-PANW"
-  enabled = true
-}
