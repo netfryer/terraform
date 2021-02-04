3d65ddc1f2d725c75368614736f86af8cc5b62e9
netfryer <78517116+netfryer@users.noreply.github.com>
2021-02-03 23:29:24 -0600

    Update providers.tf

--- azure/transit_2fw_2spoke_common/providers.tf
+++ azure/transit_2fw_2spoke_common/providers.tf
@@ -3,9 +3,6 @@ terraform {
 }
 
 provider "azurerm" {
-  #version         = "= 1.41"
-  #subscription_id = var.subscription_id
-  #client_id       = var.client_id
-  #client_secret   = var.client_secret
-  #tenant_id       = var.tenant_id
+version = "=2.0.0"
+features {}
 }
